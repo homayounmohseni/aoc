06511cbf6f929f9cb372c247b5aeb43229ed6fb1
Homayoun Mohseni <mohseni.homayoun@gmail.com>
2022-12-07 03:09:42 +0330

    solve part1 day 6 2022

---
+++ 2022/06/comm_device.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+const vsize = 4;
+
+func main() {
+	var line string
+	fmt.Scanln(&line);
+	
+	var prevChars [vsize]rune
+	for i := 0; i < vsize; i++ {
+		prevChars[i] = 0;
+	}
+
+	for ind, c := range line {
+		prevChars = shiftRight(prevChars, c);
+		if ind > 3 && isVectorOK(prevChars) {
+			fmt.Println(ind + 1)
+			return
+		}
+	}
+}
+
+func shiftRight(charVector [vsize]rune, sin rune) [vsize]rune {
+	for i := 0; i < vsize - 1; i++ {
+		charVector[i] = charVector[i + 1];
+	}
+	charVector[vsize - 1] = sin;
+	return charVector
+}
+
+func isVectorOK(charVector [vsize]rune) bool {
+	m := make(map[rune]bool, vsize)
+
+	for _, v := range charVector {
+		if m[v] {
+			return false;
+		}
+		m[v] = true;
+	}
+	return true;
+}
+
+
+		
