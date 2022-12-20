04f62afe32e4c1273d81714cd0a5d72f3cfde03b
Homayoun Mohseni <mohseni.homayoun@gmail.com>
2022-12-20 18:25:56 +0330

    solve part 2 day 10 2022

--- 2022/10/crt.cpp
+++ 2022/10/crt.cpp
@@ -5,6 +5,9 @@
 
 using namespace std;
 
+const int SCREEN_HEIGHT = 6;
+const int SCREEN_WIDTH = 40;
+
 int main() {
 
 	vector<string> instructions;
@@ -18,10 +21,14 @@ int main() {
 	for (int i = 20; i <=220; i += 40) {
 		measure_time.push_back(i);
 	}
+	measure_time.push_back(260);
 
 	int time = 1;
 	auto measure_time_it = measure_time.begin();
 	int x_val = 1;
+
+
+	vector<vector<char>> screen(SCREEN_HEIGHT, vector<char>(SCREEN_WIDTH, '.'));
 
 	for (int i = 0; i < instructions.size() && measure_time_it != measure_time.end(); i++) {
 		const auto& instruction = instructions[i];
@@ -47,9 +54,39 @@ int main() {
 			signal_strengths.push_back(*measure_time_it * new_x_val);
 			measure_time_it++;
 		}
+
+		for (int j = time; j < new_time; j++) {
+			int pixel_y = (j - 1) / SCREEN_WIDTH;
+			int pixel_x = (j - 1) % SCREEN_WIDTH;
+
+			if (pixel_y <= SCREEN_HEIGHT - 1) {
+				if (pixel_x >= x_val - 1 && pixel_x <= x_val + 1) {
+					screen[pixel_y][pixel_x] = '#';
+				}
+				else {
+					screen[pixel_y][pixel_x] = '.';
+				}
+			}
+		}
 		x_val = new_x_val;
 		time = new_time;
 	}
+
+	{
+		int j = time;
+		int pixel_y = (j - 1) / SCREEN_WIDTH;
+		int pixel_x = (j - 1) % SCREEN_WIDTH;
+
+		if (pixel_y <= SCREEN_HEIGHT - 1) {
+			if (pixel_x >= x_val - 1 && pixel_x <= x_val + 1) {
+				screen[pixel_y][pixel_x] = '#';
+			}
+			else {
+				screen[pixel_y][pixel_x] = '.';
+			}
+		}
+	}
+
 
 
 	int result = 0;
@@ -58,6 +95,13 @@ int main() {
 	}
 	cout << result << "\n";
 
+
+	for (int i = 0; i < SCREEN_HEIGHT; i++) {
+		for (int j = 0; j < SCREEN_WIDTH; j++) {
+			cout << screen[i][j];
+		}
+		cout << "\n";
+	}
 
 	return 0;
 }
