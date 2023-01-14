7f0da95b0691ea0a5609ae17a67387296f2b9d53
Homayoun Mohseni <mohseni.homayoun@gmail.com>
2023-01-15 01:40:04 +0330

    solve part 2 day 14 2022

--- 2022/14/regolith.go
+++ 2022/14/regolith.go
@@ -13,7 +13,12 @@ import (
 type Pair[T any] [2]T
 type Position Pair[int]
 type Path []Position
-type Cave map[Position]int8
+
+type Cave struct {
+	m           map[Position]int8
+	floorY      int
+	floorExists bool
+}
 
 var NoMoreInts = errors.New("no more integers found in string")
 var SandStartingPosition = Position{500, 0}
@@ -31,8 +36,40 @@ func main() {
 
 	cave := InitCave(paths)
 	highestY := findDeepestRockY(cave)
+
+	var caveFloored Cave
+	CopyCave(&caveFloored, &cave)
+	caveFloored.floorExists = true
+	caveFloored.floorY = highestY + 2
+
 	turns := simulate(cave, highestY)
+	turnsFloored := simulateFloored(caveFloored)
+
 	fmt.Println(turns)
+	fmt.Println(turnsFloored)
+}
+
+func (c *Cave) Demap(pos Position) int8 {
+	if c.floorExists && pos[1] == c.floorY {
+		return rock
+	}
+	if v, ok := c.m[pos]; ok {
+		return v
+	}
+	return air
+}
+
+func (c *Cave) Map(pos Position, v int8) {
+	c.m[pos] = v
+}
+
+func CopyCave(dst, src *Cave) {
+	dst.floorY = src.floorY
+	dst.floorExists = src.floorExists
+	dst.m = make(map[Position]int8, len(src.m))
+	for k, v := range src.m {
+		dst.m[k] = v
+	}
 }
 
 func simulate(cave Cave, deepestRockY int) int {
@@ -46,8 +83,26 @@ func simulate(cave Cave, deepestRockY int) int {
 			} else if nextPos[1] > deepestRockY {
 				return i
 			}
-			cave[curPos] = air
-			cave[nextPos] = sand
+			cave.Map(curPos, air)
+			cave.Map(nextPos, sand)
+			curPos = nextPos
+		}
+	}
+}
+
+func simulateFloored(cave Cave) int {
+	for i := 0; ; i++ {
+		curPos := SandStartingPosition
+		for {
+			nextPos := calculateNextPosition(curPos, cave)
+			if nextPos == curPos {
+				if curPos == SandStartingPosition {
+					return i + 1
+				}
+				break
+			}
+			cave.Map(curPos, air)
+			cave.Map(nextPos, sand)
 			curPos = nextPos
 		}
 	}
@@ -61,7 +116,7 @@ func calculateNextPosition(curPos Position, cave Cave) Position {
 	}
 
 	for _, pos := range possiblePositions {
-		if v, ok := cave[pos]; !ok || (ok && v == air) {
+		if cave.Demap(pos) == air {
 			return pos
 		}
 	}
@@ -70,7 +125,9 @@ func calculateNextPosition(curPos Position, cave Cave) Position {
 }
 
 func InitCave(paths []Path) Cave {
-	cave := make(Cave)
+	cave := Cave{
+		m:           make(map[Position]int8),
+	}
 	for pathNum, path := range paths {
 		var prev Position
 		if len(path) != 0 {
@@ -83,14 +140,14 @@ func InitCave(paths []Path) Cave {
 				from := MinInt(cur[1], prev[1])
 				to := MaxInt(cur[1], prev[1])
 				for y := from; y <= to; y++ {
-					cave[Position{x, y}] = rock
+					cave.Map(Position{x, y}, rock)
 				}
 			} else if cur[1] == prev[1] {
 				y := cur[1]
 				from := MinInt(cur[0], prev[0])
 				to := MaxInt(cur[0], prev[0])
 				for x := from; x <= to; x++ {
-					cave[Position{x, y}] = rock
+					cave.Map(Position{x, y}, rock)
 				}
 			} else {
 				log.Fatal(fmt.Errorf("bad input usage: prev: %v, cur: %v, pathNum: %v, i: %v",
@@ -104,7 +161,7 @@ func InitCave(paths []Path) Cave {
 
 func findDeepestRockY(c Cave) int {
 	maxY := 0
-	for k, v := range c {
+	for k, v := range c.m {
 		if v == rock {
 			maxY = MaxInt(maxY, k[1])
 		}
